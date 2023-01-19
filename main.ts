89b0b758a01a2bb2821651221088f234ca25a69e
Marlon Kieninger <marlon.kieninger@gmx.de>
2023-01-19 14:23:29 +0100

    initial stand

---
+++ main.ts
@@ -0,0 +1,23 @@
+
+var button = document.getElementById("button-add");
+
+if (button !== null) {
+    button.addEventListener("click", function () {
+        var infoText = document.getElementById("p-paragraph")
+        if (!infoText) {
+            return;
+        }
+        var inputBox = document.getElementById("input-artikel") as HTMLInputElement;
+
+        var liste = document.getElementById("ul-einkaufsliste");
+        var neuerListenpunkt = document.createElement("li")
+        if (liste == null) {
+            infoText.innerHTML = "Fehler!"
+        } else {
+            liste.appendChild(neuerListenpunkt)
+            infoText.innerHTML = ""
+        }
+        neuerListenpunkt.innerHTML = inputBox.value
+        inputBox.value= ""
+    });
+}
